4ccf61d6ef86bcd5c09b8583d2161be19a25d860
Minhkuku <minhprokute90258@gmail.com>
2022-08-31 18:27:11 +0700

    update

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(morgan("common"));
+
+app.use("/", async (req, res) => {
+  res.send("oke chưa");
+});
+app.listen(8800, () => {
+  console.log("đã kết nối cỗng 8800");
+});
